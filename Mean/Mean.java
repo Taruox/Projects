1c8929ef2610420906612208d4de2fe997037d69
Exovolt <ampexfire@gmail.com>
2013-10-29 08:56:16 -0600

    Projects

---
+++ Mean/Mean.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+public class Mean {
+
+	private static Random r = new Random(5345);
+
+	public static void main(String[] args) {
+		int[] randomArray = makeRandom(1000, 100);
+		System.out.println("The average of the array is: ");
+		System.out.println(mean(randomArray) + ".");
+
+		System.out.println("The average from index 40 to index 45 is ");
+		System.out.println(meanOfRange(randomArray. 40, 45) + ".");
+
+	}
+
+	public static double mean(int[] a) {
+
+	}
+
+	public static double meanOfRange(int[] a, int start, int end) {
+
+
+	}
+
+	public static int[] makeRandom(int size, int range) {
+		int[] a = new int[size];
+
+		for(int i = 0; i < a.length; i++){
+			a[i] = r.nextInt(range+1);
+		}
+
+		return a;
+	}
+}
---
+++ Rainfall/Rainfall.java
@@ -0,0 +1,15 @@
+class Rainfall {
+
+
+	public static void main(String[] args) {
+			double april 12;
+			double may 14;
+			double june 8;
+			double avg = (april + may + june) / 3.0;
+			System.out.println("\tRainfall for April: " + april);
+			System.out.println("\tRainfall for May: " + may);
+			System.out.println("\tRainfall for June: " + june);
+			System.out.println("\tAverage rainfall: " + avg);
+		}
+	}
+}
